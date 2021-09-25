ae146591d2d402d79d7af4ae19d0ca7949b68ce6
GraceOyiza <graceoyiza13@gmail.com>
2021-09-25 15:54:32 +0100

    Fix Linter Error

--- spec/requests/not_found_spec.rb
+++ spec/requests/not_found_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "NotFound Route", type: :request do
-  describe "GET /index" do
+RSpec.describe 'NotFound Route', type: :request do
+  describe 'GET /index' do
     it 'return 404 is route isnt found' do
       get '/unknown'
       body = JSON.parse(response.body)
